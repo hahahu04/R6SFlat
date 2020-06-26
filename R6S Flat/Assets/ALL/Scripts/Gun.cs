5887125f1c79ea0faa1f4434c24fd3239d4dba98
HBRYU <48374744+HBRYU@users.noreply.github.com>
2020-06-26 22:11:46 +0900

    업뎃

---
+++ R6S Flat/Assets/ALL/Scripts/Gun.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public LineRenderer lr;
+    public Color bulletColor;
+    public Transform barrelEnd;
+    public float damage;
+    public float accuracyOffset;
+    public float fireRate;
+    [Tooltip("Must be long enought to hit at least something")]
+    public float distance = 99;
+    public LayerMask hitLayers;
+    public LayerMask damageLayers;
+    [HideInInspector]
+    public float fireRate_timer;
+    public int magSize;
+    [HideInInspector]
+    public int magSize_counter;
+
+    [Tooltip("SMG, Rifle, Pistol, MG, Shotgun, Electric, None")]
+    public string ammoType;
+
+    public bool fullAuto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        magSize_counter = magSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (fullAuto)
+        {
+            if (Input.GetMouseButton(0) && magSize_counter > 0 && fireRate_timer <= 0)
+            {
+                Vector3 fireDir = Vector2.right;
+                fireDir.z += Random.Range(-accuracyOffset, accuracyOffset);
+                RaycastHit2D shot = Physics2D.Raycast(barrelEnd.position, fireDir, distance, hitLayers);
+
+                if (shot.collider != null)
+                {
+                    if (damageLayers == (damageLayers | (1 << shot.collider.gameObject.layer)))
+                    {
+                        Debug.Log("hit");
+                    }
+
+                    lr.
+                }
+            }
+        }
+        
+    }
+}
--- R6S Flat/Assets/ALL/Scripts/ShowInSight.cs
+++ R6S Flat/Assets/ALL/Scripts/ShowInSight.cs
@@ -14,7 +14,7 @@ public class ShowInSight : MonoBehaviour
     public Transform[] raycastPos;
 
     // Start is called before the first frame update
-    void Start()
+    void Start()    //----처음에 한번 실행----
     {
         sr = GetComponent<SpriteRenderer>();
         if (useAnim)
@@ -22,17 +22,31 @@ public class ShowInSight : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()   //----매 프레임마다 한번씩 실행----
     {
         bool inSight = false;
-
-        foreach(Transform pos in raycastPos)
+        /*
+        bool 타입 변수 inSight
+        inSight = false: 시야에 안보임, inSight = true: 보임
+        */
+        foreach (Transform pos in raycastPos)   //raycastPos라는 리스트 안에 있는 모든 Transform 타입 변수(pos)에 대해
         {
             RaycastHit2D cast = Physics2D.Raycast(pos.position, target.position - pos.position, UTIL.FastDist(target.position, pos.position, 0.05f), wallLayers);
-            Debug.DrawRay(pos.position, target.position - pos.position);
+            /*
+            RaycastHit2D 타입 변수 cast
+            pos의 위치에서 Transform 타입 변수 target(플레이어)의 방향으로 target(플레이어) 까지의 거리만큼
+            레이저를 wallLayers(벽 레이어)에서 발사
+             */
             if (cast.collider == null && UTIL.FastDist(target.position, pos.position, 0.05f) <= maxDist)
                 inSight = true;
+            /*
+            만약 레이저에 아무것도 닿지 안았고 발사 지점과 너무 멀지 않았다면:
+            inSight를 참으로 설정한다 => 보이는 거임
+             */
         }
+
+
+        //-------애니메이션(사라지기/보이기 효과)---------
         if (useAnim)
         {
             if (inSight)
